0f2863ea62737229919381f6cd4c0d2789f905a1
Luís Ventuzelos <luisventuzelos@gmail.com>
2023-08-09 23:34:09 +0100

    Sort assessements by type

--- src/resources/assessments/assessments.service.ts
+++ src/resources/assessments/assessments.service.ts
@@ -42,7 +42,7 @@ export class AssessmentsService {
       .createQueryBuilder('assessment')
       .leftJoinAndSelect('assessment.lesson_id', 'lesson') // Assuming the foreign key column is named 'lesson_id'
       .where('assessment.lesson_id = :lesson_id', { lesson_id })
-      .orderBy('assessment.id', 'ASC')
+      .orderBy('assessment.type', 'ASC')
       .getMany();
   }
 }
